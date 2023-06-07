dc8680dcac663dd1c79761d8c1a729b8d7d23b56
Skayu <kacperkolo64@gmail.com>
2023-06-08 00:00:11 +0200

    change adapter to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+// import node from '@astrojs/node';
+import netlify from '@astrojs/netlify/functions';
 import tailwind from "@astrojs/tailwind";
 
 
@@ -7,7 +8,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: netlify()
 });
